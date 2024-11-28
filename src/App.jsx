1307f4ebc52d87db0d962489557a0214281ec106
Harshit <harshit10541@gmail.com>
2024-11-29 01:54:06 +0530

    finished the frontend of ToDo app with React

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+import { NewTodoForm } from "./NewTodoForm";
+import { TodoList } from "./TodoList";
+
+export default function App() {
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+    if (localValue == null) return [];
+    return JSON.parse(localValue);
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function addTodo(title) {
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        {
+          id: crypto.randomUUID(),
+          title: title,
+          completed: false,
+        },
+      ];
+    });
+  }
+
+  console.log(todos);
+
+  return (
+    <>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">ToDo List</h1>
+      <TodoList toggleTodo={toggleTodo} deleteTodo={deleteTodo} todos={todos} />
+    </>
+  );
+}
